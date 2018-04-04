dd962b847ac4a79270e1a82cc4f875e31ae9ffc8
Davidson Bpe <davidsomixer@gmail.com>
2018-04-04 16:42:13 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,21 +1,21 @@
 <?php
-//CONFIGURAÇÕES SOBRE SEU SITE
+//CONFIGURAÃ‡Ã•ES SOBRE SEU SITE
 $nome_do_site="Meu site";
-$email_para_onde_vai_a_mensagem = "davidson.bp@hotmail.com";
+$email_para_onde_vai_a_mensagem = "sobaixarso@gmail.com";
 $nome_de_quem_recebe_a_mensagem = "WEB MASTER";
 $exibir_apos_enviar='enviado.html';
 
-//ESSA VARIAVEL DEFINE SE É O USUARIO QUEM DIGITA O ASSUNTO OU SE DEVE ASSUMIR O ASSUNTO DEFINIDO 
-//POR VOCÊ CASO O USUARIO DEFINA O ASSUNTO PONHA "s" NO LUGAR DE "n" E CRIE O CAMPO DE NOME 
+//ESSA VARIAVEL DEFINE SE Ã‰ O USUARIO QUEM DIGITA O ASSUNTO OU SE DEVE ASSUMIR O ASSUNTO DEFINIDO 
+//POR VOCÃŠ CASO O USUARIO DEFINA O ASSUNTO PONHA "s" NO LUGAR DE "n" E CRIE O CAMPO DE NOME 
 //'assunto' NO FORMULARIO DE ENVIO
 $assunto_digitado_pelo_usuario="n";
 
-//CONFIGURAÇOES DA MENSAGEM ORIGINAL
+//CONFIGURAÃ‡OES DA MENSAGEM ORIGINAL
 $cabecalho_da_mensagem_original="From: $name <$email>\n";
 $assunto_da_mensagem_original="Contato via formulario de email";
 $configuracao_da_mensagem_original="Enviado por:\nNome: $nome\nEmail: $email\nMensagem: $textodamensagem\nEnviado em: $date";
  
-//CONFIGURAÇÕES DA MENSAGEM DE RESPOSTA
+//CONFIGURAÃ‡Ã•ES DA MENSAGEM DE RESPOSTA
 // CASO $assunto_digitado_pelo_usuario="s" ESSA VARIAVEL RECEBERA AUTOMATICAMENTE A CONFIGURACAO
 // "Re: $assunto"
 $assunto_da_mensagem_de_resposta = "EMAIL RECEBIDO";
